9690d2b2d61f9939da4009652a0e8e8a20f840cf
tingamapuro04 <alponcemobutu100@gmail.com>
2022-06-08 16:20:52 +0300

    Linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,7 +7,6 @@ module.exports = {
     static: './dist',
   },
   plugins: [
-    new ESLintPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
